4ab1f30dcb60292fadc11b3376cb22a6fec2a21a
ViniciosRibeir0 <118862166+ViniciosRibeir0@users.noreply.github.com>
2022-11-22 21:13:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+
+// var n1= 10
+// var n2= 20
+
+// var soma = n2 + n1
+// var sub = n2 - n1
+// console.log('A soma entre: '+ n1 + ' e ' + n2 + ' é: ' + soma )
+// console.log(`A Subtração entre ${n2} e ${n1} é: ${sub}`)
+
+
+
+
+// var idade = 18
+
+// if(idade >=18){
+
+//     console.log ('Você é maior de idade!')
+// }else{
+// console.log('Você é menor de idade!')
+
+// }
+
+// var n1= 0
+
+// if(n1< 0){
+
+// console.log(`O número ${n1} é negativo!`)
+// }else{
+//     console.log(`O número ${n1} é Positivo!`)
+// }
+
+// var n1= 4
+
+// if(n1 % 2 == 0){
+
+//     console.log('Este número é Par!')
+// }else{
+// console.log('Este número é Impar!')
+
+// }
+
+
+
+
+// var Nota1= 10
+// var Nota2= 10
+// var Nota3= 10
+// var Nota4= 10
+
+// var Result = (Nota1 + Nota2 + Nota3 + Nota4)/4
+
+// if(Result < 5){
+
+//     console.log('Você esta reprovado! '+Result )
+// }else{
+//     console.log('Você esta Aprovado! o Babaca tirou:  '+Result )
+// }
+
